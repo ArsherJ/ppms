46520ca5ef5e4be83aa8940855b1a9004d4e9ac9
AJ Basilio <63991368+ArsherJ@users.noreply.github.com>
2024-01-20 17:30:41 +0800

    modify static files 3 hope

--- ppms_project/ppms_project/settings.py
+++ ppms_project/ppms_project/settings.py
@@ -135,13 +135,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'profilingApp/static')
-]
-MEDIA_ROOT = f'{STATICFILES_DIRS[0]}/Media'
-MEDIA_URL = '/Media/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'profilingApp/static')
+# ]
+# MEDIA_ROOT = f'{STATICFILES_DIRS[0]}/Media'
+MEDIA_URL = 'Media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
